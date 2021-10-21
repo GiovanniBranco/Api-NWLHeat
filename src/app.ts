5fda349dcf65a07d1c4a70fae4665b251d9d0516
Giovanni Branco <giovannipbranco1@gmail.com>
2021-10-21 12:26:11 -0300

    LastCommit

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,30 @@
 import "dotenv/config";
 import express from "express";
-import cors from "cors";
 import http from "http";
+import cors from "cors";
+
 import { Server } from "socket.io";
+
 import { router } from "./routes/routes";
 
 const app = express();
-app.use(express.json());
-//Crio um servidor http usando o App
-const serverHttp = http.createServer(app);
-app.use(router);
-//Habilito o uso do Cors;
-app.use(cors);
+app.use(cors());
 
-//Crio o servidor SocketIo passando o cors para aceitar requisição de qualquer origem;
+const serverHttp = http.createServer(app);
+
 const io = new Server(serverHttp, {
   cors: {
     origin: "*",
   },
 });
 
-//Monitoro a conexão no socket
 io.on("connection", (socket) => {
   console.log(`Usuário conectado no socket ${socket.id}`);
 });
+
+app.use(express.json());
+
+app.use(router);
 
 app.get("/github", (request, response) => {
   response.redirect(
