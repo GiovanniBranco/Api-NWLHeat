f377a70fa09cf048b78d16741ce6195f327abf0b
Giovanni Branco <giovannipbranco1@gmail.com>
2021-10-21 10:30:23 -0300

    Incluído o WebSocket

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,29 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
 import { router } from "./routes";
 
 const app = express();
 app.use(express.json());
-
+//Crio um servidor http usando o App
+const serverHttp = http.createServer(app);
 app.use(router);
+//Habilito o uso do Cors;
+app.use(cors);
+
+//Crio o servidor SocketIo passando o cors para aceitar requisição de qualquer origem;
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*",
+  },
+});
+
+//Monitoro a conexão no socket
+io.on("connection", (socket) => {
+  console.log(`Usuário conectado no socket ${socket.id}`);
+});
 
 app.get("/github", (request, response) => {
   response.redirect(
@@ -19,4 +37,4 @@ app.get("/signin/callback", (request, response) => {
   return response.json(code);
 });
 
-app.listen(4000, () => console.log("Server is runnig on PORT 4000"));
+export { serverHttp, io };
